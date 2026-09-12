8954b3cded55244ba27c90f2f5fa0c8aaedf269d
CelesteNostdlib <rnostdlib@gmail.com>
2026-09-11 18:02:39 -0700

    refactor: optimize logging and string formatting functions for improved clarity and efficiency

--- include/string.h
+++ include/string.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "types.h"
+#include <stdarg.h>
 
 INT32 AnsiToWide(const CHAR *ansi, PWCHAR wide, INT32 wideSize);
 BOOL AsciiEquals(const CHAR *left, const CHAR *right);
 __SIZE_TYPE__ strlen(const CHAR *s);
 __SIZE_TYPE__ wcslen(const WCHAR *s);
+INT32 Format(PCHAR s, SIZE_T size, const PCHAR format, ...);
+
